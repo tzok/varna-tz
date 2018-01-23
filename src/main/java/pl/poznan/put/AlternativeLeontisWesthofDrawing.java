ba98305bbe737e2554d344c98a5244c980f1b533
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2018-01-23 09:53:43 +0100

    Refactoring

--- src/main/java/pl/poznan/put/AlternativeLeontisWesthofDrawing.java
+++ src/main/java/pl/poznan/put/AlternativeLeontisWesthofDrawing.java
@@ -52,24 +52,15 @@ public final class AlternativeLeontisWesthofDrawing {
       final double thickness,
       final double side,
       final boolean isCis) {
+    // calculate radius
+    final double diameter = (side * Math.sqrt(2)) + (2.0 * thickness);
+    final double radius = diameter / 2.0;
+
     // draw a circle
     final Point2D.Double center =
         new Point2D.Double((orig.x + dest.x) / 2.0, (orig.y + dest.y) / 2.0);
-    final double diameter = (side * Math.sqrt(2)) + (2.0 * thickness);
-    final double radius = diameter / 2.0;
     out.fillCircle(center.x, center.y, radius, thickness, Color.WHITE);
     out.drawCircle(center.x, center.y, radius, thickness);
-
-    // calculate rotation of regular coordinate system and vector between points
-    final Point2D.Double vectorHorizontal = new Point2D.Double(1, 0);
-    final Point2D.Double vector = new Point2D.Double(dest.x - orig.x, dest.y - orig.y);
-    final double dotProduct = (vector.x * vectorHorizontal.x) + (vector.y * vectorHorizontal.y);
-    final double magnitudeHorizontal =
-        Math.sqrt(
-            (vectorHorizontal.x * vectorHorizontal.x) + (vectorHorizontal.y * vectorHorizontal.y));
-    final double magnitude = Math.sqrt((vector.x * vector.x) + (vector.y * vector.y));
-    final double cosine = dotProduct / (magnitudeHorizontal * magnitude);
-    final double angleRadians = StrictMath.acos(cosine);
 
     // initial coordinates = half the side along the (0,0) point
     final Point2D.Double[] square = {
@@ -79,20 +70,10 @@ public final class AlternativeLeontisWesthofDrawing {
       new Point2D.Double(side / 2.0, -side / 2.0),
     };
 
-    // rotate & translate coordinates
-    for (final Point2D.Double point : square) {
-      AlternativeLeontisWesthofDrawing.rotate(point, angleRadians);
-      AlternativeLeontisWesthofDrawing.translate(point, center);
-    }
-
-    // draw square
-    final double[] x = {square[0].x, square[1].x, square[2].x, square[3].x};
-    final double[] y = {square[0].y, square[1].y, square[2].y, square[3].y};
-    if (isCis) {
-      out.fillPolygon(x, y, out.getCurrentColor());
-    } else {
-      out.drawPolygon(x, y, thickness);
-    }
+    // draw everything
+    final double angle = AlternativeLeontisWesthofDrawing.calculateRotationAngle(orig, dest);
+    AlternativeLeontisWesthofDrawing.transformPoints(angle, center, square);
+    AlternativeLeontisWesthofDrawing.drawPolygon(out, square, thickness, isCis);
   }
 
   private static void drawTriangleInCircle(
@@ -102,7 +83,7 @@ public final class AlternativeLeontisWesthofDrawing {
       final double thickness,
       final double side,
       final boolean isCis) {
-    // calculate triangle properties
+    // calculate radius
     final double height = (side * Math.sqrt(3.0)) / 2.0;
     final double radius = ((2.0 * height) / 3.0) + thickness;
 
@@ -112,38 +93,17 @@ public final class AlternativeLeontisWesthofDrawing {
     out.fillCircle(center.x, center.y, radius, thickness, Color.WHITE);
     out.drawCircle(center.x, center.y, radius, thickness);
 
-    // calculate rotation of regular coordinate system and vector between points
-    final Point2D.Double vectorHorizontal = new Point2D.Double(1, 0);
-    final Point2D.Double vector = new Point2D.Double(dest.x - orig.x, dest.y - orig.y);
-    final double dotProduct = (vector.x * vectorHorizontal.x) + (vector.y * vectorHorizontal.y);
-    final double magnitudeHorizontal =
-        Math.sqrt(
-            (vectorHorizontal.x * vectorHorizontal.x) + (vectorHorizontal.y * vectorHorizontal.y));
-    final double magnitude = Math.sqrt((vector.x * vector.x) + (vector.y * vector.y));
-    final double cosine = dotProduct / (magnitudeHorizontal * magnitude);
-    final double angleRadians = StrictMath.acos(cosine);
-
-    // initial coordinates = half the side along the (0,0) point
+    // initial coordinates = center is in 2/3 of triangle height
     final Point2D.Double[] triangle = {
       new Point2D.Double(-height / 3.0, -side / 2.0),
       new Point2D.Double(-height / 3.0, side / 2.0),
       new Point2D.Double((2.0 * height) / 3.0, 0),
     };
 
-    // rotate & translate coordinates
-    for (final Point2D.Double point : triangle) {
-      AlternativeLeontisWesthofDrawing.rotate(point, angleRadians);
-      AlternativeLeontisWesthofDrawing.translate(point, center);
-    }
-
-    // draw triangle
-    final double[] x = {triangle[0].x, triangle[1].x, triangle[2].x};
-    final double[] y = {triangle[0].y, triangle[1].y, triangle[2].y};
-    if (isCis) {
-      out.fillPolygon(x, y, out.getCurrentColor());
-    } else {
-      out.drawPolygon(x, y, thickness);
-    }
+    // draw everything
+    final double angle = AlternativeLeontisWesthofDrawing.calculateRotationAngle(orig, dest);
+    AlternativeLeontisWesthofDrawing.transformPoints(angle, center, triangle);
+    AlternativeLeontisWesthofDrawing.drawPolygon(out, triangle, thickness, isCis);
   }
 
   private static void drawCircleInSquare(
@@ -178,6 +138,43 @@ public final class AlternativeLeontisWesthofDrawing {
       final double unit,
       final boolean isCis) {}
 
+  /**
+   * Calculate rotation of regular coordinate system and a vector between given points.
+   *
+   * @param orig First point.
+   * @param dest Second point.
+   * @return An angle between X-axis and given vector.
+   */
+  private static double calculateRotationAngle(
+      final Point2D.Double orig, final Point2D.Double dest) {
+    final Point2D.Double vectorHorizontal = new Point2D.Double(1, 0);
+    final Point2D.Double vector = new Point2D.Double(dest.x - orig.x, dest.y - orig.y);
+    final double dotProduct = (vector.x * vectorHorizontal.x) + (vector.y * vectorHorizontal.y);
+    final double magnitudeHorizontal =
+        Math.sqrt(
+            (vectorHorizontal.x * vectorHorizontal.x) + (vectorHorizontal.y * vectorHorizontal.y));
+    final double magnitude = Math.sqrt((vector.x * vector.x) + (vector.y * vector.y));
+    final double cosine = dotProduct / (magnitudeHorizontal * magnitude);
+    return StrictMath.acos(cosine);
+  }
+
+  /**
+   * Rotate & translate coordinates.
+   *
+   * @param rotationAngle Angle of rotation.
+   * @param translationVector 2D vector of translation.
+   * @param points Array of points to be transformed.
+   */
+  private static void transformPoints(
+      final double rotationAngle,
+      final Point2D.Double translationVector,
+      final Point2D.Double[] points) {
+    for (final Point2D.Double point : points) {
+      AlternativeLeontisWesthofDrawing.rotate(point, rotationAngle);
+      AlternativeLeontisWesthofDrawing.translate(point, translationVector);
+    }
+  }
+
   private static void rotate(final Point2D.Double point, final double angleRadians) {
     final double cosine = StrictMath.cos(angleRadians);
     final double sine = StrictMath.sin(angleRadians);
@@ -190,6 +187,35 @@ public final class AlternativeLeontisWesthofDrawing {
   private static void translate(final Point2D.Double point, final Point2D.Double vector) {
     point.x += vector.x;
     point.y += vector.y;
+  }
+
+  /**
+   * Draw a polygon filled or with just lines depending on whether the interaction is cis or trans.
+   *
+   * @param out An instance of secondary structure drawer.
+   * @param points A set of points.
+   * @param thickness A parameter of line thickness.
+   * @param isCis A parameter deciding if the polygon is filled (true) or just lines (false).
+   */
+  private static void drawPolygon(
+      final SecStrDrawingProducer out,
+      final Point2D.Double[] points,
+      final double thickness,
+      final boolean isCis) {
+    // draw square
+    final double[] x = new double[points.length];
+    final double[] y = new double[points.length];
+
+    for (int i = 0; i < points.length; i++) {
+      x[i] = points[i].x;
+      y[i] = points[i].y;
+    }
+
+    if (isCis) {
+      out.fillPolygon(x, y, out.getCurrentColor());
+    } else {
+      out.drawPolygon(x, y, thickness);
+    }
   }
 
   private AlternativeLeontisWesthofDrawing() {
